ba20dec3a33b4c01854f51088de12bd39e6df6f4
Django-explorer <bariy.litievich@gmail.com>
2020-12-16 22:45:34 +0300

    Добавьте файлы проекта.

---
+++ DZ_ALL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DZ_ALL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vse dz v otdel'nych vetkah");
+        }
+    }
+}
